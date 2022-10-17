3d5142a4bacdd5411b2c5cd6fb20a662eb39252b
Ransome-Kuti Omowale <omowaleransomekuti@gmail.com>
2022-10-17 14:27:15 -0700

    Add printf.c and header file

---
+++ printf.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _printf - a function that produces output according to a format
+ * write output to stdout, the standard output stream
+ * @format: is a character string. The format string is composed of zero
+ * or more directives to handle the following conversion specifiers: c s %
+ * Return: the number of characters printed (excluding the null byte used
+ * to end output to strings)
+ */
+int _printf(const char *format, ...)
+{
+	int counter;
+
+	char c;
+
+	va_list arg;
+
+	va_start(arg, format);
+
+	counter = 0;
+
+	for (; *format != '\0'; format++)
+	{
+		if (*format != '%')
+		{
+			c = *format;
+			write(1, &c, 1);
+			counter++;
+		}
+	}
+	return (counter);
+}
